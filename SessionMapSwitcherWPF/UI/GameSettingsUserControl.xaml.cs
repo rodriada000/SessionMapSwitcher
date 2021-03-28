7c19ed3a4bc814ac31ffb23a818fd661f020f5bd
Adam <rodriada000@gmail.com>
2021-03-27 21:09:24 -0700

    update: video settings in ui
    - fix bug related to opening mod unlocker for newer version
    - add video settings to UI
    - make texture replacer async and disable button while replacing

--- SessionMapSwitcherWPF/UI/GameSettingsUserControl.xaml.cs
+++ SessionMapSwitcherWPF/UI/GameSettingsUserControl.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using SessionMapSwitcherCore.Classes;
 using SessionModManagerCore.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SessionModManagerWPF.UI
 {
@@ -22,7 +10,7 @@ namespace SessionModManagerWPF.UI
     /// </summary>
     public partial class GameSettingsUserControl : UserControl
     {
-        private readonly GameSettingsViewModel ViewModel;
+        public readonly GameSettingsViewModel ViewModel;
 
         public GameSettingsUserControl()
         {
