82e2111f428a3b86e96e909096f0d0115252b238
Adam <rodriada000@gmail.com>
2024-08-10 15:08:24 -0400

    check for mod file conflicts

---
+++ SessionModManagerCore/Classes/FileConflict.cs
@@ -0,0 +1,18 @@
+﻿namespace SessionModManagerCore.Classes
+{
+    public class FileConflict
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// name of asset that conflicts with <see cref="ExistingAssetName"/>
+        /// </summary>
+        public string AssetName { get; set; }
+
+        /// <summary>
+        /// name of asset that is already enabled
+        /// </summary>
+        public string ExistingAssetName { get; set; }
+    }
+}
